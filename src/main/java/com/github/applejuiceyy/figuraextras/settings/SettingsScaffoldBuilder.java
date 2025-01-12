c237932e694652094ddf050923ef51689227375b
applejuiceyy <30875598+applejuiceyy@users.noreply.github.com>
2025-01-12 00:45:23 +0000

    add missing import

--- src/main/java/com/github/applejuiceyy/figuraextras/settings/SettingsScaffoldBuilder.java
+++ src/main/java/com/github/applejuiceyy/figuraextras/settings/SettingsScaffoldBuilder.java
@@ -5,6 +5,7 @@ import com.github.applejuiceyy.figuraextras.util.Event;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.internal.Streams;
 import com.google.gson.stream.JsonWriter;
 import net.minecraft.util.Tuple;
